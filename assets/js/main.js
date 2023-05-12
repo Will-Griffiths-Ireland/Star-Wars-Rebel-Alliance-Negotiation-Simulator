67fd920ad3ec0838cb2fa4a1cdb83cb21de24136
Will-Griffiths-Ireland <williamgriffithsireland@hotmail.com>
2023-05-12 18:49:21 +0000

    Add - Function to add targets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 
-
 const gamePlayArea = document.getElementById("gamePlayArea");
 
 // ###### HELPER FUNCTIONS ######
@@ -37,7 +36,7 @@ function addBackground(){
 
 
 
-function addProp(relX, relY, type, scale){
+function addProp(relX, relY, type, scale, destructable){
 
     let imgPath = "";
 
@@ -52,13 +51,13 @@ function addProp(relX, relY, type, scale){
             imgPath = "./assets/sprites/junkpile.webp";
         break;
         case "lamp":
-            imgPath = "./assets/sprites/lamp.webp";
+            imgPath = "./assets/sprites/lamp1.webp";
         break;
         case "lamp2":
             imgPath = "./assets/sprites/lamp2.webp";
         break;
         default:
-            imgPath = "./assets/sprites/rebel_supply_crate";
+            imgPath = "./assets/sprites/rebel_supply_crate.webp";
         }
 
     const asset = document.createElement('img');
@@ -68,15 +67,71 @@ function addProp(relX, relY, type, scale){
     asset.style.left = relX + "vw";
     asset.style.width = scale + "vw";
     asset.style.height = "auto";
-    asset.style.zIndex = scale + 100;
+    asset.style.zIndex = relY;
     asset.classList = "dropIn";
-    asset.addEventListener("click", destroyTarget);
+    if(destructable){
+        asset.addEventListener("click", destroyTarget);
+    }
+    
     gamePlayArea.appendChild(asset);
 
 
 }
 
 // ADD TARGTES
+
+function addTarget(relX, relY, type, scale, destructable, motionType, zIndex){
+
+    let imgPath = "";
+
+    switch(type) {
+        case "droid1":
+            imgPath = "./assets/sprites/bad_droid";
+            break;
+        case "trooper":
+            imgPath = "./assets/sprites/StormTrooper.webp";
+            break;
+        case "droid2":
+            imgPath = "./assets/sprites/battledroid.webp";
+        break;
+        case "vader":
+            imgPath = "./assets/sprites/vader.webp";
+        break;
+        case "ewok":
+            imgPath = "./assets/sprites/pissed_ewok.webp";
+        break;
+        default:
+            imgPath = "./assets/sprites/StormTrooper.webp";
+        }
+
+    const asset = document.createElement('img');
+    asset.src = imgPath;
+    asset.style.position = "absolute";
+    asset.style.top = relY + "vh";
+    asset.style.left = relX + "vw";
+    asset.style.width = scale + "vw";
+    asset.style.height = "auto";
+    if(zIndex){
+        asset.style.zIndex = zIndex;
+    }
+    else{
+        asset.style.zIndex = relY;
+    }
+    
+    asset.classList = "dropIn target";
+    
+    if(destructable){
+        asset.addEventListener("click", destroyTarget);
+    }
+
+    setTimeout(() => {
+        asset.classList.add(`${motionType}`)
+    }, 1000);
+    
+    gamePlayArea.appendChild(asset);
+
+
+}
 
 // DESTROY TARGET
 
@@ -87,7 +142,14 @@ function destroyTarget(e){
     target.removeEventListener("click", destroyTarget);
     target.style.animationDelay = "0ms";
     target.classList.remove("dropIn");
-    target.classList.add("destroyTarget");
+    if(target.classList.contains("target")){
+        target.classList.add("destroyTarget2");
+    }
+    else{
+        target.classList.add("destroyTarget");
+    }
+    
+    
 
 
 }
@@ -111,7 +173,7 @@ function scatterAssets(count) {
         top = randomNumber(50, (90 - randWidth)) + "vh";
         let delay = randomNumber(0, 2000); //generate a delay for the animation
         const asset = document.createElement('img');
-        asset.src = "./assets/sprites/pissed_ewok.webp";
+        asset.src = "./assets/sprites/vader.webp";
         asset.style.position = "absolute";
         asset.style.animationDelay = delay + "ms";
         asset.style.top = top;
@@ -126,7 +188,7 @@ function scatterAssets(count) {
         setTimeout(() => {
             asset.classList.add("dancing");
             asset.classList.add("dodge");
-        }, 1000 + delay);
+        }, 1000);
     }
 
     
@@ -168,10 +230,23 @@ function scatterBoxes(count) {
 addBackground();
 // scatterAssets(65);
 // scatterBoxes(15);
-addProp(25, 85, "box", 5);
-addProp(30, 85, "box", 5);
-addProp(35, 85, "box", 5);
+addProp(20, 75, "box", 7 , true);
+addTarget(25, 70, "trooper", 5, true, "dancing");
+addProp(30, 77, "junk", 7, true);
+addProp(40, 75, "box", 7, true);
 
-addProp(45, 60, "box", 5);
-addProp(50, 60, "box", 5);
-addProp(55, 60, "box", 5);
+addProp(40, 70, "box", 5, true);
+addTarget(43, 62, "ewok", 6, true, "dancing", 80);
+addProp(43, 72, "box", 6, true);
+addProp(46, 70, "box", 5, true);
+addProp(45, 60, "drum", 4, true);
+addTarget(50, 62, "ewok", 6, true, "vibrating", 80);
+
+addProp(50, 60, "box", 4, false);
+addTarget(48, 58, "trooper", 3, true, "dodging");
+addProp(55, 60, "box", 4, true);
+addProp(55, 12, "lamp", 4, true);
+addTarget(55, 62, "droid2", 15, true, "vibrating");
+addTarget(60, 70, "droid2", 8, true, "dancing");
+addTarget(50, 70, "vader", 8, true, "dodging",90);
+
